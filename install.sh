f39e3a9e33492fa4007d8314c9817cc9c0a6b363
nnisarggada <gadannisarg@gmail.com>
2023-08-13 00:15:42 +0530

    gtklock

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ mkdir ~/.icons
 mkdir ~/.local
 mkdir ~/.local/bin
 
-yay -S --overwrite --needed --noconfirm ncdu bluez bluez-libs bluez-utils git github-cli papirus-icon-theme pavucontrol blueman qt5ct qt5-styleplugins unzip zip firefox zsh alacritty pcmanfm htop neofetch brightnessctl light ntfs-3g udisks2 gvfs ttf-ms-fonts neovim nodejs npm lua python-pillow zathura ttf-joypixels ttf-jetbrains-mono-nerd zathura-pdf-mupdf python-pip mlocate zsh-syntax-highlighting libnotify jq sxiv webapp-manager cliphist waybar-hyprland-git wofi wofi-emoji notification-daemon swaync noto-fonts-emoji lxsession grimblast-git nwg-look-bin yad sox mint-themes bibata-cursor-theme gtklock eww-wayland xdg-desktop-portal-hyprland swww
+yay -S --overwrite --needed --noconfirm ncdu bluez bluez-libs bluez-utils git github-cli papirus-icon-theme pavucontrol blueman qt5ct qt5-styleplugins unzip zip firefox zsh alacritty thunar htop neofetch brightnessctl light ntfs-3g udisks2 gvfs ttf-ms-fonts neovim nodejs npm lua python-pillow zathura ttf-joypixels ttf-jetbrains-mono-nerd zathura-pdf-mupdf python-pip mlocate zsh-syntax-highlighting libnotify jq sxiv webapp-manager cliphist waybar-hyprland-git wofi wofi-emoji notification-daemon swaync noto-fonts-emoji lxsession grimblast-git nwg-look-bin yad sox sweet-gtk-theme-dark bibata-cursor-theme gtklock eww-wayland xdg-desktop-portal-hyprland swww
 
 sudo cp -r .config/* ~/.config/
 sudo cp -r .local/bin/* ~/.local/bin/
