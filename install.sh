cb2fd1222f7fc6196d21fb6af8b39f3c305535de
nnisarggada <gadannisarg@gmail.com>
2023-08-22 23:40:23 +0530

    caps remap

--- install.sh
+++ install.sh
@@ -9,7 +9,18 @@ mkdir ~/.icons
 mkdir ~/.local
 mkdir ~/.local/bin
 
-yay -S --overwrite --needed --noconfirm ncdu bluez bluez-libs bluez-utils git github-cli papirus-icon-theme pavucontrol blueman qt5ct qt5-styleplugins unzip zip firefox zsh alacritty thunar htop neofetch brightnessctl light ntfs-3g udisks2 gvfs ttf-ms-fonts neovim nodejs npm lua python-pillow zathura ttf-joypixels ttf-jetbrains-mono-nerd zathura-pdf-mupdf python-pip mlocate zsh-syntax-highlighting libnotify jq sxiv webapp-manager cliphist waybar-hyprland-git wofi wofi-emoji wofi-calc notification-daemon swaync noto-fonts-emoji lxsession grimblast-git nwg-look-bin yad sox mint-themes sweet-gtk-theme-dark bibata-cursor-theme gtklock eww-wayland xdg-desktop-portal-hyprland swww
+programs=("ncdu" "bluez" "bluez-libs" "bluez-utils" "git" "github-cli" "papirus-icon-theme" "pavucontrol" "blueman" "qt5ct" "qt5-styleplugins" "unzip" "zip" "firefox" "zsh" "alacritty" "thunar" "htop" "neofetch" "brightnessctl" "light" "ntfs-3g" "udisks2" "gvfs" "ttf-ms-fonts" "neovim" "nodejs" "npm" "lua" "python-pillow" "zathura" "ttf-joypixels" "ttf-jetbrains-mono-nerd" "zathura-pdf-mupdf" "python-pip" "mlocate" "zsh-syntax-highlighting" "libnotify" "jq" "sxiv" "webapp-manager" "cliphist" "waybar-hyprland-git" "wofi" "wofi-emoji" "wofi-calc" "notification-daemon" "swaync" "noto-fonts-emoji" "lxsession" "grimblast-git" "nwg-look-bin" "yad" "sox" "mint-themes" "sweet-gtk-theme-dark" "bibata-cursor-theme" "gtklock" "eww-wayland" "xdg-desktop-portal-hyprland" "swww")
+
+for program in "${programs[@]}"; do
+    if ! yay -Q "$program" >/dev/null 2>&1; then
+        echo "Installing $program..."
+        yay -S "$program"
+        echo "$program installed successfully!"
+    else
+        echo "$program is already installed."
+    fi
+done
+
 
 sudo cp -r .config/* ~/.config/
 sudo cp -r .local/* ~/.local/
