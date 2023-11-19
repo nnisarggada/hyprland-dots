9a67a5135fd910bb472573fd12b7a44acaac0179
nnisarggada <gadannisarg@gmail.com>
2023-11-19 22:51:13 +0530

    sddm theme

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ else
   yay
 fi
 
-programs=("wl-clipboard" "wireplumber" "qt5-wayland" "qt6-wayland" "evtest" "less" "wget" "ncdu" "bluez" "bluez-libs" "bluez-utils" "git" "github-cli" "papirus-icon-theme" "pavucontrol" "blueman" "qt5ct" "qt5-styleplugins" "unzip" "zip" "firefox" "zsh" "alacritty" "thunar" "htop" "neofetch" "brightnessctl" "light" "ntfs-3g" "udisks2" "udiskie" "gvfs" "ttf-ms-fonts" "neovim" "nodejs" "npm" "lua" "python-pillow" "zathura" "ttf-joypixels" "ttf-jetbrains-mono-nerd" "zathura-pdf-mupdf" "python-pip" "mlocate" "zsh-syntax-highlighting" "libnotify" "jq" "sxiv" "webapp-manager" "waybar-hyprland-git" "wofi" "wofi-emoji" "wofi-calc" "notification-daemon" "swaync" "noto-fonts-emoji" "lxsession" "grimblast-git" "nwg-look-bin" "bibata-cursor-theme" "gtklock" "xdg-desktop-portal-hyprland" "swww" "network-manager-applet" "copyq" "hyprpicker-git" "xwaylandvideobridge-cursor-mode-2-git" "mission-center")
+programs=("sddm" "sddm-sugar-candy-git" "wl-clipboard" "wireplumber" "qt5-wayland" "qt6-wayland" "evtest" "less" "wget" "ncdu" "bluez" "bluez-libs" "bluez-utils" "git" "github-cli" "papirus-icon-theme" "pavucontrol" "blueman" "qt5ct" "qt5-styleplugins" "unzip" "zip" "firefox" "zsh" "alacritty" "thunar" "htop" "neofetch" "brightnessctl" "light" "ntfs-3g" "udisks2" "udiskie" "gvfs" "ttf-ms-fonts" "neovim" "nodejs" "npm" "lua" "python-pillow" "zathura" "ttf-joypixels" "ttf-jetbrains-mono-nerd" "zathura-pdf-mupdf" "python-pip" "mlocate" "zsh-syntax-highlighting" "libnotify" "jq" "sxiv" "webapp-manager" "waybar-hyprland-git" "wofi" "wofi-emoji" "wofi-calc" "notification-daemon" "swaync" "noto-fonts-emoji" "lxsession" "grimblast-git" "nwg-look-bin" "bibata-cursor-theme" "gtklock" "xdg-desktop-portal-hyprland" "swww" "network-manager-applet" "copyq" "hyprpicker-git" "xwaylandvideobridge-cursor-mode-2-git" "mission-center")
 
 for program in "${programs[@]}"; do
     if ! yay -Q "$program" >/dev/null 2>&1; then
@@ -42,6 +42,7 @@ sudo cp -r Music ~/
 sudo cp .zshrc ~/
 
 sudo cp environment /etc/
+sudo cp sddm.conf /usr/lib/sddm/sddm.conf.d/default.conf
 
 sudo chown $USER ~/ -R
 chsh -s /usr/bin/zsh
