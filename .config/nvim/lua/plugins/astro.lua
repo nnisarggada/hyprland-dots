6bae4b4756919b42ce2833ca4570fddb7da15fa6
nnisarggada <gadannisarg@gmail.com>
2023-11-20 12:17:55 +0530

    lazy nvim

---
+++ .config/nvim/lua/plugins/astro.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "astro",
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        astro = {},
+      },
+    },
+  },
+}
