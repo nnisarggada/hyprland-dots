26b909fe683a2b334505a9bc18274c64242c0314
nnisarggada <gadannisarg@gmail.com>
2024-01-17 01:10:48 +0530

    sanity

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ else
 	yay
 fi
 
-programs=("adwaita-dark" "wl-clipboard" "wireplumber" "pamixer" "qt5-wayland" "qt6-wayland" "evtest" "less" "wget" "ncdu" "bluez" "bluez-libs" "bluez-utils" "git" "github-cli" "papirus-icon-theme" "pavucontrol" "blueman" "qt5ct" "qt5-styleplugins" "unzip" "zip" "firefox" "zsh" "alacritty" "thunar" "htop" "neofetch" "brightnessctl" "light" "ntfs-3g" "udisks2" "udiskie" "gvfs" "ttf-ms-fonts" "neovim" "nodejs" "npm" "lua" "python-pillow" "zathura" "ttf-joypixels" "ttf-jetbrains-mono-nerd" "zathura-pdf-mupdf" "python-pip" "mlocate" "zsh-syntax-highlighting" "libnotify" "jq" "sxiv" "webapp-manager" "waybar-hyprland-git" "wofi" "wofi-emoji" "wofi-calc" "notification-daemon" "swaync" "avizo" "noto-fonts-emoji" "lxsession" "grimblast-git" "nwg-look-bin" "bibata-cursor-theme" "gtklock" "xdg-desktop-portal-hyprland" "swww" "network-manager-applet" "copyq" "hyprpicker-git" "xwaylandvideobridge-cursor-mode-2-git" "mission-center")
+programs=("flatpak" "adwaita-dark" "wl-clipboard" "wireplumber" "pamixer" "qt5-wayland" "qt6-wayland" "evtest" "less" "wget" "ncdu" "bluez" "bluez-libs" "bluez-utils" "git" "github-cli" "papirus-icon-theme" "pavucontrol" "blueman" "qt5ct" "qt5-styleplugins" "unzip" "zip" "firefox" "zsh" "alacritty" "thunar" "htop" "neofetch" "brightnessctl" "light" "ntfs-3g" "udisks2" "udiskie" "gvfs" "ttf-ms-fonts" "neovim" "nodejs" "npm" "lua" "python-pillow" "zathura" "ttf-joypixels" "ttf-jetbrains-mono-nerd" "zathura-pdf-mupdf" "python-pip" "mlocate" "zsh-syntax-highlighting" "libnotify" "jq" "sxiv" "webapp-manager" "waybar-hyprland-git" "wofi" "wofi-emoji" "wofi-calc" "notification-daemon" "swaync" "avizo" "noto-fonts-emoji" "lxsession" "grimblast-git" "nwg-look-bin" "bibata-cursor-theme" "gtklock" "xdg-desktop-portal-hyprland" "swww" "network-manager-applet" "copyq" "hyprpicker-git" "xwaylandvideobridge-cursor-mode-2-git" "mission-center" "google-chrome")
 
 for program in "${programs[@]}"; do
 	if ! yay -Q "$program" >/dev/null 2>&1; then
@@ -39,6 +39,20 @@ sudo cp -r Pictures ~/
 sudo cp -r Music ~/
 sudo cp .zshrc ~/
 sudo cp .gtkrc-2.0 ~/
+
+swww init &
+swww img ~/Pictures/Wallpapers/what-will-you-say-today.jpg -t grow --transition-pos top &
+
+gsettings set org.gnome.desktop.interface clock-format '12h'
+gsettings set org.gnome.desktop.interface clock-show-date true
+gsettings set org.gnome.desktop.interface clock-show-weekday true
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.gnome.desktop.interface cursor-theme 'Bibata-Modern-Classic'
+gsettings set org.gnome.desktop.interface document-font-name 'Cantarell 12'
+gsettings set org.gnome.desktop.interface font-name 'Cantarell 12'
+gsettings set org.gnome.desktop.interface monospace-font-name 'Source Code Pro 12'
+gsettings set org.gnome.desktop.interface gtk-theme 'Colloid-Dark'
+gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
 
 sudo cp etc/environment /etc/
 
