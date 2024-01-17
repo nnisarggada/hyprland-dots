1793d73efdac1002f792d2d48b4a0fe6d9696194
nnisarggada <gadannisarg@gmail.com>
2024-01-17 09:04:52 +0530

    nvim

---
+++ .config/nvim/lua/nnisarggada/core/options.lua
@@ -0,0 +1,43 @@
+local opt = vim.opt -- for conciseness
+
+-- undo file
+opt.undofile = true
+
+-- line numbers
+opt.relativenumber = true -- show relative line numbers
+opt.number = true -- shows absolute line number on cursor line (when relative number is on)
+
+-- tabs & indentation
+opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
+opt.shiftwidth = 2 -- 2 spaces for indent width
+opt.expandtab = true -- expand tab to spaces
+opt.autoindent = true -- copy indent from current line when starting new one
+
+-- line wrapping
+opt.wrap = true -- enable line wrapping
+
+-- search settings
+opt.ignorecase = true -- ignore case when searching
+opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+
+-- cursor line
+opt.cursorline = true -- highlight the current cursor line
+
+-- appearance
+
+-- turn on termguicolors for nightfly colorscheme to work
+-- (have to use iterm2 or any other true color terminal)
+opt.signcolumn = "yes" -- show sign column so that text doesn't shift
+
+-- backspace
+opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+
+-- clipboard
+opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+
+-- split windows
+opt.splitright = true -- split vertical window to the right
+opt.splitbelow = true -- split horizontal window to the bottom
+
+vim.cmd([[hi NvimTreeNormal guibg=NONE ctermbg=NONE]])
+vim.cmd([[autocmd BufWritePre * lua vim.lsp.buf.format()]])
