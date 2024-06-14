de38fcd313fd2736982ee7f3abee4d8f65883245
nnisarggada <gadannisarg@gmail.com>
2024-06-14 16:17:48 +0530

    fix

--- .config/nvim/lua/nnisarggada/plugins-setup.lua
+++ .config/nvim/lua/nnisarggada/plugins-setup.lua
@@ -115,7 +115,10 @@ return packer.startup(function(use)
   use("chrisbra/Colorizer")
 
   -- codeium
-  use("Exafunction/codeium.vim")
+  use({
+    "Exafunction/codeium.vim",
+    commit = "289eb724e5d6fab2263e94a1ad6e54afebefafb2",
+  })
 
   -- astro.build
   use("wuelnerdotexe/vim-astro")
