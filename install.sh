400cc5d62a07bd95efb0742bca5f6a2ac850f4ea
nnisarggada <gadannisarg@gmail.com>
2024-11-21 18:27:16 +0530

    sddm

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ else
 fi
 echo ""
 
-required_programs=("adwaita-dark" "alacritty" "blueman" "bluez" "bluez-libs" "bluez-utils" "brightnessctl" "evtest" "librewolf-bin" "flatpak" "git" "grimblast" "hypridle" "hyprlock" "hyprpaper" "hyprpicker" "jq" "libnotify" "libva" "libva-nvidia-driver-git" "light" "lxsession" "mlocate" "ncdu" "network-manager-applet" "nodejs" "noto-fonts-emoji" "npm" "ntfs-3g" "nwg-look" "pamixer" "gruvbox-dark-icons-gtk" "pavucontrol" "qt5-wayland" "qt5ct" "qt6ct" "qt6gtk2" "qt5-styleplugins" "qt6-wayland" "sxiv" "thunar" "ttf-jetbrains-mono-nerd" "ttf-joypixels" "unzip" "zip" "unrar" "cabextract" "udiskie" "udisks2" "waybar" "wireplumber" "pipewire-pulse" "wl-clipboard" "xdg-desktop-portal-hyprland" "xwaylandvideobridge" "zathura" "zathura-pdf-mupdf" "zsh" "zsh-syntax-highlighting" "github-cli" "chromium" "neofetch" "neovim" "python-pywal" "v4l2loopback-dkms" "onlyoffice-bin" "yt-dlp" "silicon" "dash" "lua" "go" "python-pip" "man" "ripgrep" "linux-headers" "p7zip" "auto-cpufreq" "mpd" "mpv" "rsync" "gvfs" "pass" "gcr" "bemenu-wayland" "gruvbox-dark-gtk" "bibata-cursor-theme-bin" "ttf-courier-prime" "ttf-rasa" "ls-of" "fzf")
+required_programs=("adwaita-dark" "alacritty" "blueman" "bluez" "bluez-libs" "bluez-utils" "brightnessctl" "evtest" "librewolf-bin" "flatpak" "git" "grimblast" "hypridle" "hyprlock" "hyprpaper" "hyprpicker" "jq" "libnotify" "libva" "libva-nvidia-driver-git" "light" "lxsession" "mlocate" "ncdu" "network-manager-applet" "nodejs" "noto-fonts-emoji" "npm" "ntfs-3g" "nwg-look" "pamixer" "gruvbox-dark-icons-gtk" "pavucontrol" "qt5-wayland" "qt5ct" "qt6ct" "qt6gtk2" "qt5-styleplugins" "qt6-wayland" "sxiv" "thunar" "ttf-jetbrains-mono-nerd" "ttf-joypixels" "unzip" "zip" "unrar" "cabextract" "udiskie" "udisks2" "waybar" "wireplumber" "pipewire-pulse" "wl-clipboard" "xdg-desktop-portal-hyprland" "xwaylandvideobridge" "zathura" "zathura-pdf-mupdf" "zsh" "zsh-syntax-highlighting" "github-cli" "chromium" "neofetch" "neovim" "python-pywal" "v4l2loopback-dkms" "onlyoffice-bin" "yt-dlp" "silicon" "dash" "lua" "go" "python-pip" "man" "ripgrep" "linux-headers" "p7zip" "auto-cpufreq" "mpd" "mpv" "rsync" "gvfs" "pass" "gcr" "bemenu-wayland" "gruvbox-dark-gtk" "bibata-cursor-theme-bin" "ttf-courier-prime" "ttf-rasa" "ls-of" "fzf" "sddm")
 
 echo ""
 echo "Checking and installing required programs..."
@@ -47,6 +47,8 @@ sudo cp -r pix ~/
 sudo cp .zshrc ~/
 sudo cp .gtkrc-2.0 ~/
 
+sudo cp -r usr/* /usr/
+
 mkdir ~/pix/ss
 
 sudo chown $USER ~/ -R
@@ -55,6 +57,9 @@ hyprpaper &
 sudo systemctl enable bluetooth
 systemctl --user enable mpd
 sudo modprobe v4l2loopback
+
+sudo systemctl disable display-manager
+sudo systemctl enable sddm
 
 sudo npm i -g pnpm
 pnpm setup
@@ -74,8 +79,6 @@ gsettings set org.gnome.desktop.interface font-name 'Cantarell 12'
 gsettings set org.gnome.desktop.interface monospace-font-name 'Courier Prime Pro 12'
 gsettings set org.gnome.desktop.interface gtk-theme 'gruvbox-dark-gtk'
 gsettings set org.gnome.desktop.interface icon-theme 'gruvbox-dark-icons-gtk'
-
-sudo cp -r etc/* /etc/
 
 sudo rm -rf /bin/sh
 sudo ln -s dash /bin/sh
