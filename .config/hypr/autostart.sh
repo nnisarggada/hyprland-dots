d64840c1ad9a502daf231b0d125c420298a27d57
nnisarggada <gadannisarg@gmail.com>
2024-11-22 17:41:33 +0530

    echo

--- .config/hypr/autostart.sh
+++ .config/hypr/autostart.sh
@@ -13,20 +13,23 @@
 
 ~/.local/bin/batmon -n -L 20 -s ~/.local/share/sounds/notification.ogg -v 75 &
 ~/.local/bin/drink_water &
-bluetoothctl power off &
 
-wal -i ~/pix/walls/flower.jpg &
+bluetoothctl power off &
 
 hyprpaper &
 hypridle &
 
 ~/.config/waybar/start &
-~/.config/swaync/start &
 
 lxsession &
 
 udiskie &
 
-brightnessctl -r
+brightnessctl -r &
+
+pactl unload-module module-echo-cancel &
+pactl load-module module-echo-cancel aec_method=webrtc source_name=echocancel sink_name=echocancel1 &
+pactl set-default-source echocancel &
+pactl set-default-sink echocancel1 &
 
 eval "sleep 2; hyprctl reload" &
