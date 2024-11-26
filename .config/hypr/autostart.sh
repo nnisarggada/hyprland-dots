3abd6c03e3d985817d315572cbdaa3c09ceeb7a9
nnisarggada <gadannisarg@gmail.com>
2024-11-26 13:40:34 +0530

    fix

--- .config/hypr/autostart.sh
+++ .config/hypr/autostart.sh
@@ -27,9 +27,4 @@ udiskie &
 
 brightnessctl -r &
 
-pactl unload-module module-echo-cancel &
-pactl load-module module-echo-cancel aec_method=webrtc source_name=echocancel sink_name=echocancel1 &&
-pactl set-default-source echocancel &&
-pactl set-default-sink echocancel1 &
-
 eval "sleep 2; hyprctl reload" &
